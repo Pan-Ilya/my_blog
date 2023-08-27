201ec07258e6554e2eefd31dfd38b8b5ca00deb3
Ilya Pankratov <pankratovilyasergeevich@gmail.com>
2023-08-27 17:14:49 +0300

    Added views class for main page + add routing file.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .models import *
+
 
 # Create your views here.
+
+
+class MainPageView(View):
+    def get(self, request):
+        data = Post.objects.all()
+        return HttpResponse(data)
