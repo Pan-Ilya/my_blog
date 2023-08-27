82afe3985d780027e4f63b7d46b60752fed84ec5
Ilya Pankratov <pankratovilyasergeevich@gmail.com>
2023-08-27 17:51:40 +0300

    Added pagination.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
@@ -10,5 +11,10 @@ from .models import *
 
 class MainPageView(View):
     def get(self, request):
-        data = Post.objects.all()
-        return render(request, 'blog/index.html', {'posts': data})
+        posts = Post.objects.all()
+        pagintor = Paginator(posts, 3)
+
+        page_number = request.GET.get('page')
+        page_obj = pagintor.get_page(page_number)
+
+        return render(request, 'blog/index.html', {'page_obj': page_obj})
